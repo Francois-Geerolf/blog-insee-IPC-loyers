e52d65902675f9a56894a8eca9ce3d9706029289
François Geerolf <100227019+Francois-Geerolf@users.noreply.github.com>
2025-07-17 14:44:59 +0200

    update

--- figure1.R
+++ figure1.R
@@ -1,33 +1,68 @@
 library(tidyverse)
-figure1_idbanks <- c("001759970", "001763530")
+library(rsdmx)
+library(scales)
+library(viridis)
 
+# ---- Paramètres ----
+
+idbank_codes <- c("001759970", "001763530")
 min_date <- as.Date("1990-01-01")
 max_date <- as.Date("2021-01-01")
 
-figure1 <- paste(figure1_idbanks, collapse = "+") |>
-  paste0("https://www.bdm.insee.fr/series/sdmx/data/SERIES_BDM/", i = _) |>
-  rsdmx::readSDMX() |>
+# ---- Construction de l'URL ----
+
+url <- paste0(
+  "https://www.bdm.insee.fr/series/sdmx/data/SERIES_BDM/",
+  paste(idbank_codes, collapse = "+")
+)
+
+# ---- Import et transformation des données ----
+
+figure1 <- url |>
+  readSDMX() |>
   as_tibble() |>
-  transmute(TITLE_FR = str_remove(TITLE_FR, "Ensemble des ménages"),
-            TITLE_FR = str_extract(TITLE_FR, "Loyers effectifs|Ensemble"),
-            date = as.Date(paste0(TIME_PERIOD, "-01")),
-            OBS_VALUE = as.numeric(OBS_VALUE)) %>%
-  filter(date <= max_date, date >= min_date) %>%
-  group_by(TITLE_FR) %>%
-  arrange(date) %>%
-  mutate(OBS_VALUE = 100*OBS_VALUE/OBS_VALUE[1])
+  transmute(
+    TITLE_FR = str_remove(TITLE_FR, "Ensemble des ménages"),
+    TITLE_FR = str_extract(TITLE_FR, "Loyers effectifs|Ensemble"),
+    TITLE_FR = factor(TITLE_FR,
+                      levels = c("Loyers effectifs", "Ensemble"),
+                      labels = c("Loyers effectifs", "Ensemble de l'IPC")),
+    date = as.Date(paste0(TIME_PERIOD, "-01")),
+    OBS_VALUE = as.numeric(OBS_VALUE)
+  ) |>
+  filter(date >= min_date, date <= max_date) |>
+  group_by(TITLE_FR) |>
+  arrange(date) |>
+  mutate(OBS_VALUE = 100 * OBS_VALUE / OBS_VALUE[1]) |>
+  ungroup()
 
-figure1 %>%
-  ggplot() + ylab("Indice des prix") + xlab("") + theme_minimal() +
-  geom_line(aes(x = date, y = OBS_VALUE, color = TITLE_FR)) +
-  scale_color_manual(values = viridis::viridis(3)[1:2]) +
-  scale_x_date(breaks = seq(1920, 2100, 5) %>% paste0("-01-01") %>% as.Date,
-               labels = date_format("%Y")) +
-  theme(legend.position = c(0.7, 0.2),
-        legend.title = element_blank()) +
-  scale_y_log10(breaks = seq(0, 200, 5),
-                labels = dollar_format(accuracy = 1, prefix = ""))
+# ---- Graphique ----
 
+ggplot(figure1, aes(x = date, y = OBS_VALUE, color = TITLE_FR)) +
+  geom_line(size = 1) +
+  scale_color_manual(
+    values = viridis(3)[1:2]
+  ) +
+  scale_x_date(
+    breaks = seq(1990, 2021, 5) |> paste0("-01-01") |> as.Date(),
+    labels = date_format("%Y")
+  ) +
+  scale_y_log10(
+    breaks = seq(80, 200, 5),
+    labels = dollar_format(accuracy = 1, prefix = "")
+  ) +
+  labs(
+    x = NULL,
+    y = "Indice des prix"
+  ) +
+  theme_minimal(base_size = 13) +
+  theme(
+    legend.position = c(0.7, 0.2),
+    legend.title = element_blank()
+  )
 
-ggsave("figure1.png", width = 1.25*6, height = 1.25*3.375)
-ggsave("figure1.pdf", width = 1.25*6, height = 1.25*3.375)
+# ---- Export graphique ----
+
+ggsave("figure1.png", width = 7.5, height = 4.2)
+ggsave("figure1.pdf", width = 7.5, height = 4.2, device = cairo_pdf)
+
--- figure2.R
+++ figure2.R
@@ -1,31 +1,66 @@
 library(tidyverse)
-figure2_idbanks <- c("010567012", "010567056")
+library(rsdmx)
+library(scales)
+library(zoo)
+library(viridis)
 
+# ---- Paramètres ----
+
+idbank_codes <- c("010567012", "010567056")
 min_date <- as.Date("1998-01-01")
 max_date <- as.Date("2022-01-01")
 
-figure2 <- paste(figure2_idbanks, collapse = "+") |>
-  paste0("https://www.bdm.insee.fr/series/sdmx/data/SERIES_BDM/", i = _) |>
-  rsdmx::readSDMX() |>
+# ---- Construction de l’URL ----
+
+url <- paste0(
+  "https://www.bdm.insee.fr/series/sdmx/data/SERIES_BDM/",
+  paste(idbank_codes, collapse = "+")
+)
+
+# ---- Import et transformation des données ----
+
+figure2 <- url |>
+  readSDMX() |>
   as_tibble() |>
-  transmute(TITLE_FR = str_extract(TITLE_FR, "Paris|France métropolitaine"),
-            date = zoo::as.Date(zoo::as.yearqtr(TIME_PERIOD, format = "%Y-Q%q")),
-            OBS_VALUE = as.numeric(OBS_VALUE)) %>%
-  filter(date <= max_date, date >= min_date) %>%
-  group_by(TITLE_FR) %>%
-  arrange(date) %>%
-  mutate(OBS_VALUE = 100*OBS_VALUE/OBS_VALUE[1])
+  transmute(
+    TITLE_FR = str_extract(TITLE_FR, "Paris|France métropolitaine"),
+    TITLE_FR = factor(TITLE_FR, levels = c("Paris", "France métropolitaine")),
+    date = as.Date(as.yearqtr(TIME_PERIOD, format = "%Y-Q%q")),
+    OBS_VALUE = as.numeric(OBS_VALUE)
+  ) |>
+  filter(date >= min_date, date <= max_date) |>
+  group_by(TITLE_FR) |>
+  arrange(date) |>
+  mutate(OBS_VALUE = 100 * OBS_VALUE / OBS_VALUE[1]) |>
+  ungroup()
 
-figure2 %>%
-  ggplot + geom_line(aes(x = date, y = OBS_VALUE, color = TITLE_FR)) +
-  theme_minimal()  +
-  scale_x_date(breaks = as.Date(paste0(seq(1960, 2050, 2), "-01-01")),
-               labels = scales::date_format("%Y")) +
-  scale_color_manual(values = viridis::viridis(3)[1:2]) +
-  theme(legend.position = c(0.25, 0.85),
-        legend.title = element_blank()) +
-  xlab("") + ylab("Indice des prix des logements anciens") +
-  scale_y_log10(breaks = seq(0, 7000, 50))
+# ---- Graphique ----
 
-ggsave("figure2.png", width = 1.25*6, height = 1.25*3.375)
-ggsave("figure2.pdf", width = 1.25*6, height = 1.25*3.375)
+ggplot(figure2, aes(x = date, y = OBS_VALUE, color = TITLE_FR)) +
+  geom_line(size = 1) +
+  scale_color_manual(
+    values = viridis(3)[1:2],
+    labels = c("Paris", "France métropolitaine")
+  ) +
+  scale_x_date(
+    breaks = seq(1998, 2025, 2) |> paste0("-01-01") |> as.Date(),
+    labels = date_format("%Y")
+  ) +
+  scale_y_log10(
+    breaks = seq(50, 7000, by = 50)
+  ) +
+  labs(
+    x = NULL,
+    y = "Indice des prix des logements anciens"
+  ) +
+  theme_minimal(base_size = 13) +
+  theme(
+    legend.position = c(0.25, 0.85),
+    legend.title = element_blank()
+  )
+
+# ---- Export ----
+
+ggsave("figure2.png", width = 7.5, height = 4.2)
+ggsave("figure2.pdf", width = 7.5, height = 4.2, device = cairo_pdf)
+
